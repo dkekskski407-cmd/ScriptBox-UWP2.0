3cee1d4185f470132a4faef84de1700e6f5e04f1
dkekskski407-cmd <dkekskski407@gmail.com>
2026-01-18 15:19:00 -0300

    Add MainPage.xaml.cs for MainPage class

---
+++ ci-app/ScriptBox/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+using Windows.UI.Xaml.Controls;
+
+namespace ScriptBox
+{
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
